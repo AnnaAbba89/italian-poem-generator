8e2c1c2042d6c30425f97d92adf4284f615d2714
AnnaAbba89 <anna.abba89@gmail.com>
2024-01-16 14:22:30 +0100

    Added basic form handling and typewriter effect in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
+  alert("generating poem");
+  new Typewriter("#poem", {
+    strings: ["Nel mezzo del cammin di nostra vita"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
